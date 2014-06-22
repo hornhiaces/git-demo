032cd7d97adc3ac553367539318abfa45884c987
Larry Salinas <larrysalinas@mac.com>
2014-06-22 15:03:13 -0500

    Initial Commit

---
+++ PredatorDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PredatorDemo
+//
+//  Created by Larry Salinas Mac 1 on 6/22/14.
+//  Copyright (c) 2014 Xcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PredatorDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PredatorDemo
+//
+//  Created by Larry Salinas Mac 1 on 6/22/14.
+//  Copyright (c) 2014 Xcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
